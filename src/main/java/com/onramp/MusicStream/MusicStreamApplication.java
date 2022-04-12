0626de4fc489edc940ef2e13e86b3dc356074392
magilo <53962625+magilo@users.noreply.github.com>
2022-04-12 15:51:11 -0400

    test commit

---
+++ src/main/java/com/onramp/MusicStream/MusicStreamApplication.java
@@ -0,0 +1,16 @@
+package com.onramp.MusicStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicStreamApplication.class, args);
+
+		System.out.println("Welcome to Spring Boot"); //placeholder to test run
+	}
+
+
+}
---
+++ src/test/java/com/onramp/MusicStream/MusicStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onramp.MusicStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
